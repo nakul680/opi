1fcf2ebb2ace1888df2484387c472de9e00cc3d1
Nakul Santhosh <santhosh@faccts.de>
2025-11-12 16:41:13 +0100

    initial draft of property results from_json() method

--- src/opi/output/models/json/property/property_results.py
+++ src/opi/output/models/json/property/property_results.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from opi.output.models.base.get_item import GetItem
 from opi.output.models.json.property.properties.calc_info import CalcInfo
 from opi.output.models.json.property.properties.calc_status import (
@@ -8,6 +10,7 @@ from opi.output.models.json.property.properties.calc_time import (
 )
 from opi.output.models.json.property.properties.geometries import Geometries
 from opi.output.models.json.property.properties.pal import PalFlags
+import json
 
 
 class PropertyResults(GetItem):
@@ -31,3 +34,26 @@ class PropertyResults(GetItem):
     calculation_timings: CalculationTiming | None = None
     pal_flags: PalFlags | None = None
     geometries: list[Geometries] | None = None
+
+    @classmethod
+    def from_json(cls, file_path:Path | str) -> "PropertyResults":
+        with open(file_path, "r") as file:
+            data = json.load(file)
+
+        calc_info = data.get("Calculation_Info",{})
+        calc_info = {k.lower(): v for k,v in calc_info.items()}
+        calc_status = data.get("Calculation_Status",{})
+        calc_status = {k.lower(): v for k,v in calc_status.items()}
+        calc_timings = data.get("Calculation_Timings",{})
+        calc_timings = {k.lower(): v for k, v in calc_timings.items()}
+        pal_flags = data.get("PAL_Flags",{})
+        pal_flags = {k.lower(): v for k, v in pal_flags.items()}
+        geometries = data.get("Geometries",[])
+
+        return cls(
+            calculation_info=CalcInfo(**calc_info),
+            calculation_status=CalculationStatus(**calc_status),
+            calculation_timings=CalculationTiming(**calc_timings),
+            pal_flags=PalFlags(**pal_flags),
+            geometries=geometries,
+        )
