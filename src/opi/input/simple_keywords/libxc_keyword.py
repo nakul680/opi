2205851c0a14083b2fa42059d7327b5482e21ceb
Nakul Santhosh <santhosh@faccts.de>
2026-02-18 11:59:04 +0100

    formatted code

--- src/opi/input/simple_keywords/libxc_keyword.py
+++ src/opi/input/simple_keywords/libxc_keyword.py
@@ -1,5 +1,3 @@
-from pygments.lexers import functional
-
 from opi.input.simple_keywords import SimpleKeyword
 
 
@@ -12,18 +10,19 @@ class LibxcKeyword(SimpleKeyword):
     functional: SimpleKeyword
         DFT functional used.
     """
+
     functional: SimpleKeyword
 
     def __init__(self, functional: SimpleKeyword):
         super().__init__(f"LibXC({functional.keyword})")
         self.functional = functional
 
-
     def __call__(self, functional: SimpleKeyword | str) -> SimpleKeyword:
         if isinstance(functional, str):
             functional = SimpleKeyword(functional)
         elif not isinstance(functional, SimpleKeyword):
             raise TypeError(
-                f"Functional '{functional}' must be a SimpleKeyword or str, got {type(functional).__name__}")
+                f"Functional '{functional}' must be a SimpleKeyword or str, got {type(functional).__name__}"
+            )
 
         return SimpleKeyword(f"LibXC({functional.keyword})")
