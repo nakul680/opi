0adf343881a44e494b3399a8bb3bbcad520fce77
Nakul Santhosh <santhosh@faccts.de>
2026-04-13 16:39:31 +0200

    changes from todays meeting

--- src/opi/input/simple_keywords/scf.py
+++ src/opi/input/simple_keywords/scf.py
@@ -5,49 +5,29 @@ from opi.input.simple_keywords.base import (
 
 __all__ = ("Scf",)
 
+class ScfThreshold(SimpleKeywordBox):
+    SLOPPYSCF = SimpleKeyword("sloppyscf", alias="sloppy")
+    """SimpleKeyword: SCF convergence threshold settings."""
+    LOOSESCF = SimpleKeyword("loosescf", alias="loose")
+    """SimpleKeyword: SCF convergence threshold settings."""
+    NORMALSCF = SimpleKeyword("normalscf", alias="normal")
+    """SimpleKeyword: SCF convergence threshold settings."""
+    STRONGSCF = SimpleKeyword("strongscf", alias="strong")
+    """SimpleKeyword: SCF convergence threshold settings."""
+    TIGHTSCF = SimpleKeyword("tightscf", alias="tight")
+    """SimpleKeyword: SCF convergence threshold settings."""
+    VERYTIGHTSCF = SimpleKeyword("verytightscf", alias="verytight")
+    """SimpleKeyword: SCF convergence threshold settings."""
+    EXTREMESCF = SimpleKeyword("extremescf", alias="extreme")
+    """SimpleKeyword: SCF convergence threshold settings."""
 
-class Scf(SimpleKeywordBox):
-    """Enum to store all simple keywords of type Scf."""
 
-    G3CONV = SimpleKeyword("3conv")
-    """SimpleKeyword: SCF solver combination."""
-    AODIISTRAH = SimpleKeyword("aodiistrah")
-    """SimpleKeyword: SCF solver combination."""
-    DIISTRAH = SimpleKeyword("diistrah")
-    """SimpleKeyword: SCF solver combination."""
-    KDIISTRAH = SimpleKeyword("kdiistrah")
-    """SimpleKeyword: SCF solver combination."""
-    DIIS = SimpleKeyword("diis")
-    """SimpleKeyword: SCF solver."""
-    NODIIS = SimpleKeyword("nodiis")
-    """SimpleKeyword: SCF solver."""
-    AODIIS = SimpleKeyword("aodiis")
-    """SimpleKeyword: SCF solver."""
-    NOAODIIS = SimpleKeyword("noaodiis")
-    """SimpleKeyword: SCF solver."""
-    KDIIS = SimpleKeyword("kdiis")
-    """SimpleKeyword: SCF solver."""
-    NOKDIIS = SimpleKeyword("nokdiis")
-    """SimpleKeyword: SCF solver."""
-    SOSCF = SimpleKeyword("soscf")
-    """SimpleKeyword: SCF solver."""
-    NOSOSCF = SimpleKeyword("nososcf")
-    """SimpleKeyword: SCF solver."""
-    TRAH = SimpleKeyword("trah")
-    """SimpleKeyword: SCF solver."""
-    NOTRAH = SimpleKeyword("notrah")
-    """SimpleKeyword: SCF solver."""
-    AUTOSTART = SimpleKeyword("autostart")
-    """SimpleKeyword: SCF initial guess start SCF from a gbw file with the same basename (default)."""
-    NOAUTOSTART = SimpleKeyword("noautostart")
-    """SimpleKeyword: SCF initial guess do not start SCF from a gbw file with the same basename."""
+class ScfGuess(SimpleKeywordBox):
     MOREAD = SimpleKeyword("moread")
     """SimpleKeyword: SCF initial guess read orbitals from gbw file."""
     EHTANO = SimpleKeyword("ehtano")
     """SimpleKeyword: SCF initial guess."""
     HCORE = SimpleKeyword("hcore")
-    """SimpleKeyword: SCF initial guess."""
-    HUECKEL = SimpleKeyword("hueckel")
     """SimpleKeyword: SCF initial guess."""
     PATOM = SimpleKeyword("patom")
     """SimpleKeyword: SCF initial guess."""
@@ -61,6 +41,64 @@ class Scf(SimpleKeywordBox):
     """SimpleKeyword: SCF initial guess."""
     SYMBREAKGUESS = SimpleKeyword("symbreakguess")
     """SimpleKeyword: SCF initial guess."""
+
+
+class ScfConvergence(SimpleKeywordBox):
+    EASYCONV = SimpleKeyword("easyconv", alias="easy")
+    """SimpleKeyword: SCF convergence strategy."""
+    NORMALCONV = SimpleKeyword("normalconv", alias="normal")
+    """SimpleKeyword: SCF convergence strategy."""
+    SLOWCONV = SimpleKeyword("slowconv", alias="slow")
+    """SimpleKeyword: SCF convergence strategy."""
+    VERYSLOWCONV = SimpleKeyword("veryslowconv", alias="veryslow")
+    """SimpleKeyword: SCF convergence strategy."""
+
+
+class ScfSolver(SimpleKeywordBox):
+    DIIS = SimpleKeyword("diis")
+    """SimpleKeyword: SCF solver."""
+    KDIIS = SimpleKeyword("kdiis")
+    """SimpleKeyword: SCF solver."""
+    SOSCF = SimpleKeyword("soscf")
+    """SimpleKeyword: SCF solver."""
+    TRAH = SimpleKeyword("trah")
+    """SimpleKeyword: SCF solver."""
+
+
+class Scf(ScfThreshold, ScfConvergence, ScfGuess, ScfSolver):
+    """Enum to store all simple keywords of type Scf."""
+
+    G3CONV = SimpleKeyword("3conv")
+    """SimpleKeyword: SCF solver combination."""
+    AODIISTRAH = SimpleKeyword("aodiistrah")
+    """SimpleKeyword: SCF solver combination."""
+    DIISTRAH = SimpleKeyword("diistrah")
+    """SimpleKeyword: SCF solver combination."""
+    KDIISTRAH = SimpleKeyword("kdiistrah")
+    """SimpleKeyword: SCF solver combination."""
+
+    NODIIS = SimpleKeyword("nodiis")
+    """SimpleKeyword: SCF solver."""
+    AODIIS = SimpleKeyword("aodiis")
+    """SimpleKeyword: SCF solver."""
+    NOAODIIS = SimpleKeyword("noaodiis")
+    """SimpleKeyword: SCF solver."""
+
+    NOKDIIS = SimpleKeyword("nokdiis")
+    """SimpleKeyword: SCF solver."""
+
+    NOSOSCF = SimpleKeyword("nososcf")
+    """SimpleKeyword: SCF solver."""
+
+    NOTRAH = SimpleKeyword("notrah")
+    """SimpleKeyword: SCF solver."""
+    AUTOSTART = SimpleKeyword("autostart")
+    """SimpleKeyword: SCF initial guess start SCF from a gbw file with the same basename (default)."""
+    NOAUTOSTART = SimpleKeyword("noautostart")
+    """SimpleKeyword: SCF initial guess do not start SCF from a gbw file with the same basename."""
+    HUECKEL = SimpleKeyword("hueckel")
+    """SimpleKeyword: SCF initial guess."""
+
     UNITMATRIXGUESS = SimpleKeyword("unitmatrixguess")
     """SimpleKeyword: SCF initial guess."""
     USEGRAMSCHMIDT = SimpleKeyword("usegramschmidt")
@@ -91,20 +129,6 @@ class Scf(SimpleKeywordBox):
     """SimpleKeyword: enable fractional occupations."""
     SCFCONVFORCED = SimpleKeyword("scfconvforced")
     """SimpleKeyword: Force SCF convergence for subsequent operations."""
-    SLOPPYSCF = SimpleKeyword("sloppyscf")
-    """SimpleKeyword: SCF convergence threshold settings."""
-    LOOSESCF = SimpleKeyword("loosescf")
-    """SimpleKeyword: SCF convergence threshold settings."""
-    NORMALSCF = SimpleKeyword("normalscf")
-    """SimpleKeyword: SCF convergence threshold settings."""
-    STRONGSCF = SimpleKeyword("strongscf")
-    """SimpleKeyword: SCF convergence threshold settings."""
-    TIGHTSCF = SimpleKeyword("tightscf")
-    """SimpleKeyword: SCF convergence threshold settings."""
-    VERYTIGHTSCF = SimpleKeyword("verytightscf")
-    """SimpleKeyword: SCF convergence threshold settings."""
-    EXTREMESCF = SimpleKeyword("extremescf")
-    """SimpleKeyword: SCF convergence threshold settings."""
     SLOPPYSCFCHECK = SimpleKeyword("sloppyscfcheck")
     """SimpleKeyword: SCF convergence threshold settings."""
     NOSLOPPYSCFCHECK = SimpleKeyword("nosloppyscfcheck")
@@ -125,14 +149,6 @@ class Scf(SimpleKeywordBox):
     """SimpleKeyword: SCF convergence threshold settings."""
     SCFCONV12 = SimpleKeyword("scfconv12")
     """SimpleKeyword: SCF convergence threshold settings."""
-    EASYCONV = SimpleKeyword("easyconv")
-    """SimpleKeyword: SCF convergence strategy."""
-    NORMALCONV = SimpleKeyword("normalconv")
-    """SimpleKeyword: SCF convergence strategy."""
-    SLOWCONV = SimpleKeyword("slowconv")
-    """SimpleKeyword: SCF convergence strategy."""
-    VERYSLOWCONV = SimpleKeyword("veryslowconv")
-    """SimpleKeyword: SCF convergence strategy."""
     DAMP = SimpleKeyword("damp")
     """SimpleKeyword: SCF settings."""
     NODAMP = SimpleKeyword("nodamp")
--- src/opi/tasks/method_settings.py
+++ src/opi/tasks/method_settings.py
@@ -1,9 +1,106 @@
 import typing
+import warnings
 
-from opi.input.simple_keywords import Dft, SimpleKeyword
+from pydantic import field_validator, model_validator
+
+from opi.input.simple_keywords import Dft, SimpleKeyword, Grid, DispersionCorrection
+from opi.input.simple_keywords.scf import ScfThreshold, ScfSolver, Scf, ScfConvergence
 from opi.tasks.task_base import MethodSettings
 
 
 class DFTSettings(MethodSettings):
-    method: typing.Annotated[SimpleKeyword, Dft]
     _name: str = "dft"
+    method: typing.Annotated[SimpleKeyword, Dft]
+    grid: typing.Annotated[SimpleKeyword, Grid] | None = None
+    scf_maxiter: typing.Annotated[int, "BlockScf", "maxiter"] | None = None
+    scf_threshold: typing.Annotated[SimpleKeyword, ScfThreshold] | None = None
+    scf_solver: typing.Annotated[SimpleKeyword, ScfSolver] | None = None
+    scf_stab: typing.Annotated[SimpleKeyword, Scf] | None = None
+    scf_conv: typing.Annotated[SimpleKeyword, ScfConvergence] | None = None
+
+
+    @field_validator("*", mode="before")
+    @classmethod
+    def validate_fields(cls, value: typing.Any, info):
+        if info.field_name == "scf_stab":
+            if value:
+                return Scf.SCFSTAB
+            else:
+                return None
+        if info.field_name == "method":
+            try:
+                new_keyword = Dft.find_keyword(value)
+            except ValueError:
+                new_keyword = cls._find_dft_disp_keyword(value)
+            return new_keyword
+        else:
+            return super().validate_fields(value, info)
+
+
+
+    @model_validator(mode="after")
+    @classmethod
+    def cross_validate(cls, data: "DFTSettings") -> "DFTSettings":
+        """
+        Cross-validation for `DftSettings`.
+        If the method keyword contains '3c', the `basis_set` attribute will be set to `None`.
+
+        The `DftSettings` object is then returned.
+        Parameters
+        ----------
+        data: DFTSettings
+            `DFTSettings` object given as input.
+
+        Returns
+        -------
+        DFTSettings
+            Cross-validated `DFTSettings` object.
+
+        """
+        if "3c" in data.method.keyword and data.basis_set:
+            warnings.warn("Basis Set will be ignored due to selection of Method", UserWarning)
+            data.basis_set = None
+
+        return data
+
+    @classmethod
+    def _find_dft_disp_keyword(cls, value: str | SimpleKeyword) -> SimpleKeyword:
+        """
+        Function to search for a `Dft` keyword along with `DispersionCorrection`.
+        In the case that an - is present in keyword, the keyword is split along the - and it is verified whether the
+        given keyword is a valid combination of `Dft` and `DispersionCorrection` keyword.
+
+        If it is , a `SimpleKeyword` object is created and returned. If not , a `ValueError` is raised.
+        Parameters
+        ----------
+        value: str | SimpleKeyword
+            The value to search for.
+
+        Returns
+        -------
+        SimpleKeyword
+            The created `SimpleKeyword` object.
+
+        Raises
+        ------
+        ValueError
+            If given value is invalid
+
+        """
+        if isinstance(value, SimpleKeyword):
+            value = value.keyword
+
+        if '-' in value:
+            try:
+                keywords = value.split('-')
+                Dft.find_keyword(keywords[0])
+                DispersionCorrection.find_keyword(keywords[1])
+
+                return SimpleKeyword(value)
+            except ValueError:
+                raise ValueError(f"Invalid Dft keyword or dispersion correction given: {value}")
+        else:
+            raise ValueError(f"Invalid Dft keyword '{value}'")
+
+
+
