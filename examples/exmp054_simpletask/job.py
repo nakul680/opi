5a8a881f5911a5fd249c82ee2448a43c696687cc
Nakul Santhosh <santhosh@faccts.de>
2026-04-22 10:07:16 +0200

    example for simple task

---
+++ examples/exmp054_simpletask/job.py
@@ -0,0 +1,45 @@
+import shutil
+import sys
+from pathlib import Path
+
+from opi.input.structures import Structure
+from opi.tasks.singlepointtask import SinglePointTask, SinglePointResults
+
+
+def run_exmp054(
+    structure: Structure | None = None, working_dir: Path | None = Path("RUN")
+) -> SinglePointResults:
+    # > recreate the working dir
+    shutil.rmtree(working_dir, ignore_errors=True)
+    working_dir.mkdir()
+
+    # > if no structure is given read structure from inp.xyz
+    if structure is None:
+        structure = Structure.from_xyz("inp.xyz")
+
+    # > set up the task
+    simple_task = SinglePointTask(method = "b3lyp",
+                                  basis_set="def2-svp",
+                                  solvation_model="cpcm",
+                                  solvent="water")
+    # > there are task and method-specific settings, these can be set through kwargs
+
+    # > run the calculation with given data
+    singlepoint_result = simple_task.run("job", structure)
+    # also possible for user to restart a calculation after changing any input options already given
+
+    # > check if the ORCA calculation terminated normally
+    if not singlepoint_result.status:
+        print("SinglePoint task failed")
+        sys.exit(1)
+
+    # > extract final energy from the `TaskResults` object
+    final_energy = singlepoint_result.final_energy
+
+    print(f"Final single point energy: {final_energy: 10f} Eh")
+
+    return singlepoint_result
+
+
+if __name__ == "__main__":
+    run_exmp054()
